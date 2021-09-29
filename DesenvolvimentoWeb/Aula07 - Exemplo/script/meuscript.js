418148183513d9f36bd3331655c629b0874b5ed1
Isabella Fagundes Barão <bellabarao@icloud.com>
2021-09-29 15:38:49 -0300

    Aula07

---
+++ DesenvolvimentoWeb/Aula07 - Exemplo/script/meuscript.js
@@ -0,0 +1,21 @@
+$(function () {
+    $("#detail_p1").hide();
+    $("#prod1").click(function () {
+    $("#detail_p1").toggle(2000);
+    });
+   });
+
+   $(function () {
+    $("#detail_p2").hide();
+    $("#prod2").click(function () {
+    $("#detail_p2").toggle(2000);
+    });
+   });
+
+   $(function () {
+    $("#detail_p3").hide();
+    $("#prod3").click(function () {
+    $("#detail_p3").toggle(2000);
+    });
+   });
+   
